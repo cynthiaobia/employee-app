87a9e05c01caee69c12a9da441bb758b825ea117
Cynthia <cynthia.obia@gmail.com>
2023-07-26 19:54:12 -0500

    started working on Employee Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import EmployeePage from './pages/EmployeePage.js';
 import HomePage from './pages/HomePage.js';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <EmployeePage />
     </div>
   );
 }
---
+++ src/pages/EmployeePage.js
@@ -0,0 +1,11 @@
+
+
+function EmployeePage(){
+    return (
+        <div>
+            <h1>Employee</h1>
+        </div>
+    )
+}
+
+export default EmployeePage;
