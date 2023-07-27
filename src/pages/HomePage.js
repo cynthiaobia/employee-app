bf85b6785f601e197aa794734e9d4f5eeed766f9
Cynthia <cynthia.obia@gmail.com>
2023-07-26 19:45:36 -0500

    added componenets to Home Page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+// import './../App.css';
+import Header from './../components/Header'
+import SearchBar from './../components/SearchBar'
+import EmployeeList from './../components/EmployeeList';
+
+function HomePage() {
+  return (
+    <div className="App">
+        <Header />
+        <SearchBar />
+        <EmployeeList />
+    </div>
+  );
+}
+
+export default HomePage;
